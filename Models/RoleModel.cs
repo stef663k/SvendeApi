4144287f096bf6c10c869e1a2c3b9837c7d5dda5
stef663k <stef663k@elev.techcollege.dk>
2025-10-23 09:47:07 +0200

    Userrole

---
+++ Models/RoleModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SvendeApi.Models;
+
+public class RoleModel
+{
+    public Guid RoleId { get; set; }
+    public string RoleName { get; set; }
+}
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string User = "User";
+    public const string Moderator = "Moderator";
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SvendeApi.Models;
+
+public class UserRole
+{
+    public Guid UserId { get; set; }
+    public UserModel User { get; set; }
+    public Guid RoleId { get; set; }
+    public RoleModel Role { get; set; }
+}
