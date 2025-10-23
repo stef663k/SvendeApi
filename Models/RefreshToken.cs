c380992487da53c08b5ecd955ae6bbc773fd81a5
stef663k <stef663k@elev.techcollege.dk>
2025-10-23 12:26:41 +0200

    string spelling error

--- Models/RefreshToken.cs
+++ Models/RefreshToken.cs
@@ -13,7 +13,7 @@ public class RefreshToken
     public string Token { get; set; } = null!;
     public DateTime ExpiresAt { get; set; }
     public DateTime CreatedAt { get; set; }
-    public striing? CreatedByIp { get; set; }
+    public string? CreatedByIp { get; set; }
     public DateTime? RevokedAt { get; set; }
     public string? RevokedByIp { get; set; }
     public string? ReplacedByToken { get; set; }
