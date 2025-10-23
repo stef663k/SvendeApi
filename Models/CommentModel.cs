a7aff3c21e3bd70d5a6e4923e59295f8cf06274e
stef663k <stef663k@elev.techcollege.dk>
2025-10-23 10:09:45 +0200

    COmmentModel is done

---
+++ Models/CommentModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SvendeApi.Models;
+
+public class CommentModel
+{
+    public Guid CommentId { get; set; }
+    public Guid PostId { get; set; }
+    public PostModel Post { get; set; }
+    public Guid AuthorId { get; set; }
+    public UserModel Author { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public Guid ParentCommentId { get; set; }
+    public CommentModel? ParentComment { get; set; }
+}
