17789f8ad865c983cbe3559502d2f56d0cf2e424
stef663k <stef663k@elev.techcollege.dk>
2025-10-24 13:50:49 +0200

    IPostService is done

---
+++ Interface/IPostService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SvendeApi.DTO;
+using SvendeApi.Models;
+
+namespace SvendeApi.Interface;
+
+public interface IPostService
+{
+    Task<PostDTO> CreateAsync(Guid userId, CreatePostDTO createPostDTO);
+    Task<PostDTO> GetAsync(Guid postId);
+    Task<IReadOnlyList<PostDTO>> GetAllAsync(int skip, int take);
+    Task<IReadOnlyList<PostDTO>> GetUserPostsAsync(Guid userId, int skip, int take);
+    Task<IReadOnlyList<PostDTO>> GetUserFeedAsync(Guid userId, int skip, int take);
+    Task<bool> DeleteAsync(Guid postId, Guid requestedUserId);
+    Task<bool> LikeAsync(Guid postId, Guid userId);
+    Task<bool> UnlikeAsync(Guid postId, Guid userId);
+}
