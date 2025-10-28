3a4f9bc659e6fafd6e3430e75eccb6eefcbd8d2d
stef663k <stef663k@elev.techcollege.dk>
2025-10-28 11:21:52 +0100

    UserService is done

--- Interface/IGenericInterface.cs
+++ Interface/IGenericInterface.cs
@@ -7,9 +7,9 @@ namespace SvendeApi.Interface;
 
 public interface IGenericInterface<T>
 {
-    Task<T> GetByIdAsync(Guid id);
-    Task<IEnumerable<T>> GetAllAsync();
-    Task<T> AddAsync(T entity);
+    Task<T> CreateAsync(T entity);
     Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(Guid id);
+    Task<T> DeleteAsync(T entity);
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> GetAllAsync();
 }
